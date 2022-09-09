3a300dd304969f72a0c114017203aa04e548b1fa
odo27 <odo27@naver.com>
2022-09-09 23:38:46 +0900

    solve image not updated
    photo taked in usb coneection, in local pc, can't show it because of not updated, it is remained 0 bytes. so i used uhubctl for powering off usb port and powering on.

--- upload_web.py
+++ upload_web.py
@@ -2,12 +2,23 @@ import requests
 import os
 import download_web
 import time
+import subprocess
 
 while True:
-    camera_path = 'D:/flir/'
+    #camera_path = 'D:/flir/'
+    #camera_path = './img_mouse/'
+    #camera_path = './img_hand/'
+    camera_path = '/run/user/1000/gvfs/mtp:host=FLIR_Systems_FLIR_Camera/Images/DCIM/100_FLIR'
     flir_camera_images = os.listdir(camera_path)
     print(flir_camera_images)
+    
+    result = subprocess.check_output(['sudo', 'uhubctl', '-a', 'off', '-p', '4', '-l', '1-1'])
+    print(result)
+    result2 = subprocess.check_output(['sudo', 'uhubctl', '-a', 'on', '-p', '4', '-l', '1-1'])
+    print(result2)
+    time.sleep(3)
 
+    
     drive_filelist = download_web.drive_filelist('admin')
 
     for filename in flir_camera_images:
@@ -19,4 +30,4 @@ while True:
             response = requests.post("http://34.64.107.120:8080/flirupload", files=files)
             print(filename, response.text)
     
-    time.sleep(100)
+    time.sleep(5)
